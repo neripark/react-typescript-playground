ead7e1a3b75fbe876c82ee9d06163529d52a477d
neripark <neripark@gmail.com>
2020-03-22 23:52:07 +0900

    tutorial: コンポジションと継承

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -6,8 +6,22 @@ type PageProps = {} & RouteComponentProps<{}>
 const Tutorial: React.FC<PageProps> = () => {
   return (
     <>
-      <h1>this is tutorial page.</h1>
+      <h1>This is tutorial page.</h1>
+      <h2>コンポジションと継承</h2>
+      <SimpleWrapper> テストのテキスト </SimpleWrapper>
+      <p>コンポジションはchildrenを受け取れるようにすることによる抽象化を行うもの。継承は有効なケースが見つかっていない。</p>
     </>
+  )
+}
+
+const SimpleWrapper: React.FC<{children: React.ReactNode; className?: string;}> = ({
+  children,
+  className
+}) => {
+  return (
+    <p className={className}>
+      {children}
+    </p>
   )
 }
 
