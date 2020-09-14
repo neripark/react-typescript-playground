a70fa61ced09e266cc3ef986eb84b5eb843452e6
neripark <neripark@gmail.com>
2020-09-14 23:22:10 +0900

    preparels data

--- src/pages/FunctionPropTest.tsx
+++ src/pages/FunctionPropTest.tsx
@@ -3,19 +3,42 @@ import { RouteComponentProps } from "react-router-dom";
 
 type PageProps = {} & RouteComponentProps<{}>;
 
-interface Props {
+// _____________________________________
+//
+interface ButtonProps {
   onClick: () => void;
-  children: string;
 }
 
-const Button: React.FC<Props> = props => (
+const Button: React.FC<ButtonProps> = props => (
   <button onClick={props.onClick} style={{ marginRight: "15px" }}>
     {props.children}
   </button>
 );
 
+// _____________________________________
+//
+interface ButtonGroupProps {
+  data: ButtonProps[];
+}
+
+const ButtonGroup: React.FC<ButtonGroupProps> = props => {
+  return (
+    <>
+      {props.data.map((element, index) => (
+        <Button key={index} onClick={element.onClick}>
+          {`ボタン${index + 1}`}
+        </Button>
+      ))}
+    </>
+  );
+};
+
+// _____________________________________
+//
 const generateLogger = (num: number) => {
-  return () => console.log(`押されたのは ${num} 番目のボタンです。`);
+  return (): void => {
+    console.log(`押されたのは ${num} 番目のボタンです。`)
+  };
 };
 
 const data = [
@@ -25,17 +48,29 @@ const data = [
   { id: 4, text: "雪" }
 ];
 
+// __________________ page component ___________________
 const FunctionPropTest: React.FC<PageProps> = () => {
+  const dataForButtonGroup: ButtonProps[] = data.map(element => ({
+    onClick: generateLogger(element.id),
+    children: element.text
+  }));
+
   return (
     <>
       <h2>FunctionPropTest</h2>
       <p>繰り返し構文の中で動的な関数生成をして大丈夫かを知りたい</p>
       <hr />
+      <h3>直接ボタンをRender</h3>
       {data.map((element, index) => (
         <Button key={index} onClick={generateLogger(element.id)}>
-          {element.text}
+          {`ボタン${index + 1}`}
         </Button>
       ))}
+      <hr />
+      <h3>データを作ってからRender</h3>
+      <ButtonGroup data={dataForButtonGroup} />
+      <hr />
+      <h3>結論：どちらも正常に動く。</h3>
     </>
   );
 };
