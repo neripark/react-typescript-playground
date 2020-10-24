f795a3d5fa7a8fb7d36d53ea4cc46998bb5beb99
neripark <neripark@gmail.com>
2020-10-24 13:21:43 +0900

    split method and refer event taget

--- src/pages/ExifReaderTest/hooks.ts
+++ src/pages/ExifReaderTest/hooks.ts
@@ -4,12 +4,17 @@ import * as ExifReader from 'exifreader';
 const useHooks = () => {
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    loadByReader(e);
+  }
+
+  const loadByReader = (e: React.ChangeEvent<HTMLInputElement>) => {
     const reader = new FileReader();
     if (e.target.files === null) return;
     const file = e.target.files[0];
-    reader.onload = () => {
-      console.log('on load kicked.');
-      const result = reader.result!;
+
+    // reader onload はイベントを受け取れる。reader 自体を読むよりそのほうがよさそう
+    reader.onload = (e: ProgressEvent<FileReader>) => {
+      const result = e.target!.result;
       const tags = ExifReader.load(result as ArrayBuffer)
       console.log(tags.Orientation);
     };
