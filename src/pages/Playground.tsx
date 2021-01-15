5b6eab7772cda438a59ad4d92bbac4ac37854dac
neripark <neripark@gmail.com>
2021-01-15 12:14:21 +0900

    test change event

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -4,13 +4,18 @@ import { RouteComponentProps } from "react-router-dom";
 type PageProps = {} & RouteComponentProps<{}>;
 
 const Playground: React.FC<PageProps> = () => {
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+  }
   return (
     <>
       <h1>playground page</h1>
+      <input
+        type="text"
+        onChange={(e) => onChange(e)}
+      />
     </>
   );
 };
-
-const FancyButton: React.FC = ({ children }) => <button>{children}</button>;
 
 export default Playground;
